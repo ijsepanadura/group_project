1a873dcb53a2a5a742c52581d318cdc36fcb9aec
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-09-25 19:57:07 +0530

    Code AppInitializer

--- public-chat/cllient-app/src/main/java/AppInitializer.java
+++ public-chat/cllient-app/src/main/java/AppInitializer.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -8,7 +12,12 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"))));
+        primaryStage.centerOnScreen();
+        primaryStage.setTitle("Login Window");
+        primaryStage.setResizable(false);
+        primaryStage.show();
 
     }
 }
